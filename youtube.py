d4b4bc15a5f96a41bd5187fec82025c8719e6cac
Abin Simon <abinsimon10@gmail.com>
2016-02-06 21:16:27 +0530

    Added functionality
    Now it can load specified number of results and also the replies to comments of a video

--- youtube.py
+++ youtube.py
@@ -1,38 +1,67 @@
-'''
-This is used to get comments of a youtube video by providing the video id
-
-Install this for it to work:
-pip install --upgrade google-api-python-client
-'''
-
 from apiclient.discovery import build
 
 #Give your api key here, you can obtain it by using the google api console (https://console.developers.google.com)
 #And btw the number of requests using an api key will be limited
-api_key = 'your-api-key'
+#Use this to install the tools required :  pip install --upgrade google-api-python-client
+
+api_key = ''	#Give your api key here
 
 #authenticate
 youtube = build('youtube', 'v3', developerKey = api_key)
 
-#request and obtain youtube comments - video_id -- id of the youtube video
-def get_youtube_comments(video_id):
+
+def get_youtube_comments(video_id, max_results = 10):
+	'''
+	request and obtain youtube comments (top level)
+	video_id -- id of the youtube video
+	result is a dictionary
+	'''
 	results = youtube.commentThreads().list(
     part="snippet",
     videoId=video_id,
-    textFormat="plainText"
+    textFormat="plainText",
+    maxResults = max_results
   ).execute()
 	return results
 
-def display_comments(comment_querry_result):
-	for item in res["items"]:
-		#author name
-		print item["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"] + " :"
-		#author comment
-		print item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-		print "\n\n"
+def get_comment_reply(parent_id, max_results = 10):
+	'''
+	Function to obtain replies to a comment on youtube video
+	parent_id -- id of the main comment
+	result is a dictionary
+	'''
+	results = youtube.comments().list(
+    part="snippet",
+    parentId=parent_id,
+    textFormat="plainText",
+    maxResults = max_results
+  ).execute()
+	return results
+
+'''
+Display functions - just for debuging and to show the usage
+'''
+
+def display_comment_parent(item):
+	print "\n\n~~~~~~~~~~~~"
+	#author name (parent)
+	print item["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"] + " :"
+	#author comment (parent)
+	print item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+
+def display_comment_repy(comment_querry_result):
+	for items in comment_querry_result["items"]:
+		print "\t-----Reply------"
+		print "\t" + items["snippet"]["authorDisplayName"]
+		print "\t" + items["snippet"]["textDisplay"]
 
 
 # Get to display comments of a video by giving the videoId(silly google, they dont have python naming scheme)
 vid = ''	#id of the youtube video
-res = get_youtube_comments(vid)
-display_comments(res)
+res_parent = get_youtube_comments(vid)
+for items in res_parent["items"]:
+	display_comment_parent(items)
+	parent_id = items["snippet"]["topLevelComment"]["id"]
+	res_child = get_comment_reply(parent_id)
+	if res_child:
+		display_comment_repy(res_child)
