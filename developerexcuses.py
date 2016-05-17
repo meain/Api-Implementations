988cdaed9fdbf4dbab17b67f19c8dd536cfe4c23
meain <abinsimon10@gmail.com>
2016-05-18 01:57:56 +0530

    Update

--- developerexcuses.py
+++ developerexcuses.py
@@ -2,15 +2,23 @@ import urllib2
 from bs4 import BeautifulSoup
 import time
 
+
 def get_quote():
     response = urllib2.urlopen('http://developerexcuses.com/')
     html = response.read()
     soup = BeautifulSoup(html, 'html.parser')
     return soup.body.div.center.a.string
 
-# while True:
-#     print get_quote()
-#     time.sleep(5)
+quotefile = open('d.txt', 'r+')
 
+while True:
+    quotefile.seek(0, 0)
+    content = quotefile.read()
+    new_quote = get_quote()
+    if new_quote not in content:
+        print new_quote
+        new_quote = '\n' + new_quote
+        quotefile.write(new_quote)
+    # print get_quote()
 
 print get_quote()
